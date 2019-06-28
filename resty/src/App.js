dbb4bdebd28c115f02abe39ffe28b255bd66643d
Alexander <maniacallion@gmail.com>
2019-06-28 15:37:06 -0500

    rest-request, rest-response, and resty components

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import Header from './components/header';
+import Resty from './components/resty';
 import Footer from './components/footer';
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <Header />
+    <Resty />
     <Footer />
     </>
   );
---
+++ resty/src/components/rest-response/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Response = props => {
+  if(!props.response){
+    return (<div>Please enter a spell and cast!</div>)
+  }
+
+  return (
+    <section className="response">
+      <h3>headers</h3>
+      <pre>
+        {JSON.stringify(props.response.headers)}
+      </pre>
+      <h3>body</h3>
+      <pre>
+        {JSON.stringify(props.response.body)}
+      </pre>
+    </section>
+  )
+};
+
+export default Response;
